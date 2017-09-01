2391b5ea273c46e02dddb5a9678cbe15526283b8
sayadaazami <sayadaazami@gmail.com>
2017-09-01 13:28:44 +0430

    base structure init

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// (JSX, id of el)
+var userName = 'Sayad Aezami';
+ReactDOM.render(
+  <h1>Empty ReactJs Project</h1>,
+  document.getElementById('app')
+);
